276ac72d807851d26f7b372dcbc0b6ec7c030b96
thamirisz <93575340+thamirisz@users.noreply.github.com>
2024-04-02 21:43:53 -0700

    day3

--- src/advent_of_code_2015/Day3/Day3.java
+++ src/advent_of_code_2015/Day3/Day3.java
@@ -7,15 +7,51 @@ import java.util.*;
 public class Day3 {
 
     class Point {
-        long x;
-        long y;
+        int x;
+        int y;
 
-        public Point(long x, long y) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
+
+        //will need equals and hashCode
+        //traditional equals is comparing object references
+        //for hashmap and hashset we need to implement our own equals method to compare based on x and y coordinates
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x &&
+                    y == point.y;
+        }
+
+        //hashCode(): compute the hash code of a Point object.
+        //Hash codes are used by hash-based collections for hashmap or hashset to efficiently organize and retrieve objects.
+        //When you store a custom object as a key in a HashMap or add it to a HashSet
+        // Java uses the object's hash code to determine
+        // where to place the object in memory or which bucket to use for storage and retrieval.
+        // Therefore, it's essential that the hashCode method be consistent with the equals method.
+        // If two objects are equal according to the equals method,
+        // they must produce the same hash code.
+        // In the overridden hashCode method,
+        // we typically combine the hash codes of the object's fields,
+        // in this case, x and y,
+        // using a suitable hashing algorithm to ensure proper distribution of hash codes across the hash table.
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
     }
     private String fileName;
+
+    Map<Point, Integer> gifts = new HashMap<>();
 
     public Day3(final String fileName) {
         this.fileName = fileName;
@@ -27,34 +63,43 @@ public class Day3 {
     }
 
     public Map<Point, Integer> deliver(String data) {
-        Map<Point, Integer> gifts = new HashMap<>();
         Point startPoint = new Point(0, 0);
         gifts.put(startPoint, 1);
-        long curr_x = 0;
-        long curr_y = 0;
+        int curX = 0;
+        int curY = 0;
 
         for (char ch : data.toCharArray()) {
             if (ch == '<') {
-                curr_x -= 1;
+                curX -= 1;
             } else if (ch == '>') {
-                curr_x += 1;
+                curX += 1;
             } else if (ch == '^') {
-                curr_y -= 1;
+                curY -= 1;
             } else {
-                curr_y += 1;
+                curY += 1;
             }
-            Point currentPoint = new Point(curr_x, curr_y);
+            Point currentPoint = new Point(curX, curY);
             gifts.put(currentPoint, gifts.getOrDefault(currentPoint, 0) + 1);
         }
         return gifts;
     }
 
     private Object[] solve(String data) {
-        int size = deliver(data).size();
-
-        Set<Point> combined = new HashSet<>(deliver(data.substring(0, data.length() / 2)).keySet());
-        combined.addAll(deliver(data.substring(data.length() / 2)).keySet());
-        return new Object[] {size, combined.size()};
+//        int size = deliver(data).size();
+        Map<Point, Integer> robotMap = new HashMap<>();
+        List<Character> santa = new ArrayList<>();
+        List<Character> robot = new ArrayList<>();
+        char[] array = data.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                santa.add(array[i]);
+            } else {
+                robot.add(array[i]);
+            }
+        }
+//        var t = deliver(santa.toString());
+//        var r = deliver(robot.toString());
+        return new Object[] {0, gifts.size()};
     }
 
     public void execute() {
